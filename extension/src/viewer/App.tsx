be6ba0849a431a8d70f028b08ba585e75b0e26e3
Paolo Simone <p.simone@live.com>
2021-10-30 09:27:53 +0200

    add alert error message

--- extension/src/viewer/App.tsx
+++ extension/src/viewer/App.tsx
@@ -2,6 +2,7 @@ import { useMemo } from "react";
 import "tailwindcss/tailwind.css";
 import { EmptyJQCommand } from "./commons/JQCommand";
 import { EmptySearch } from "./commons/Search";
+import { Alert } from "./components";
 import { useJQ, useStateObject } from "./hooks";
 import { Toolbar } from "./Toolbar";
 import { TreeViewer } from "./TreeViewer";
@@ -20,26 +21,26 @@ export function App({ jsonText, jqWasmFile }: AppProps): JSX.Element {
   const jqResult = useJQ(jqWasmFile, jsonText, jqCommandState.value);
   const [json, error] = useParseOutcome(jsonText, jqResult);
 
+  if (!json) {
+    return (
+      <div className="flex flex-col h-full font-mono">
+        {error && <Alert message={error.message} />}
+        <div className="p-3 whitespace-pre">{jsonText}</div>
+      </div>
+    );
+  }
+
+  // TODO setting text size
+
   const toolbarProps = {
     searchState: searchState,
     jqCommandState: jqCommandState,
   };
 
-  // TODO setting text size
-
-  if (!json) {
-    return (
-      <div className="flex flex-col h-full font-mono">
-        {error && <div>{error.message}</div>}
-        <div className="whitespace-pre">{jsonText}</div>
-      </div>
-    );
-  }
-
   return (
     <div className="flex flex-col h-full font-mono">
       <Toolbar {...toolbarProps} />
-      {error && <div>{error.message}</div>}
+      {error && <Alert message={error.message} />}
       <div className="flex-1 mt-1.5 pl-1.5 text-sm">
         <TreeViewer json={json} search={searchState.value} />
       </div>
--- extension/src/viewer/components/index.ts
+++ extension/src/viewer/components/index.ts
@@ -1,2 +1,3 @@
+export * from "./Alert";
 export * as Icon from "./Icon";
 export * from "./IconButton";
