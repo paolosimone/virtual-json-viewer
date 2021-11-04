47ced6420c2d741f0f4eb36b9c41ce025126abb5
Paolo Simone <p.simone@live.com>
2021-11-04 21:43:28 +0100

    add reset settings button

--- extension/src/options/App.tsx
+++ extension/src/options/App.tsx
@@ -2,7 +2,7 @@ import classNames from "classnames";
 import "tailwindcss/tailwind.css";
 import { useSettings, useTheme } from "viewer/hooks";
 import { TranslationContext, useLocalization } from "viewer/localization";
-import { resolveTextSizeClass, TextSize } from "viewer/state";
+import { DefaultSettings, resolveTextSizeClass, TextSize } from "viewer/state";
 import {
   LanguageSelect,
   NumberInput,
@@ -19,49 +19,60 @@ export function App(): JSX.Element {
     <TranslationContext.Provider value={t}>
       <div
         className={classNames(
-          "grid grid-cols-2 gap-3 p-10 dark:bg-gray-700 dark:text-gray-200",
+          "flex flex-col p-8 dark:bg-gray-700 dark:text-gray-200",
           resolveTextSizeClass(settings.textSize)
         )}
       >
-        <label>{t.settings.labels.theme}</label>
-        <ThemeSelect theme={theme} setTheme={setTheme} />
+        <div className="grid grid-cols-2 gap-3">
+          <label>{t.settings.labels.theme}</label>
+          <ThemeSelect theme={theme} setTheme={setTheme} />
 
-        <label>{t.settings.labels.language}</label>
-        <LanguageSelect language={language} setLanguage={setLanguage} />
+          <label>{t.settings.labels.language}</label>
+          <LanguageSelect language={language} setLanguage={setLanguage} />
 
-        <label>{t.settings.labels.textSize}</label>
-        <TextSizeSelect
-          textSize={settings.textSize}
-          setTextSize={(newTextSize: TextSize) =>
-            updateSettings({ textSize: newTextSize })
-          }
-        />
+          <label>{t.settings.labels.textSize}</label>
+          <TextSizeSelect
+            textSize={settings.textSize}
+            setTextSize={(newTextSize: TextSize) =>
+              updateSettings({ textSize: newTextSize })
+            }
+          />
 
-        <label>{t.settings.labels.indentation}</label>
-        <NumberInput
-          min={1}
-          value={settings.indentation}
-          setValue={(newValue: number) =>
-            updateSettings({ indentation: newValue })
-          }
-        />
+          <label>{t.settings.labels.indentation}</label>
+          <NumberInput
+            min={1}
+            value={settings.indentation}
+            setValue={(newValue: number) =>
+              updateSettings({ indentation: newValue })
+            }
+          />
 
-        <label>{t.settings.labels.searchDelay}</label>
-        <NumberInput
-          min={0}
-          value={settings.searchDelay}
-          setValue={(newValue: number) =>
-            updateSettings({ searchDelay: newValue })
-          }
-        />
+          <label>{t.settings.labels.searchDelay}</label>
+          <NumberInput
+            min={0}
+            value={settings.searchDelay}
+            setValue={(newValue: number) =>
+              updateSettings({ searchDelay: newValue })
+            }
+          />
 
-        <label>{t.settings.labels.sortKeys}</label>
-        <input
-          className="self-center"
-          type="checkbox"
-          checked={settings.sortKeys}
-          onChange={() => updateSettings({ sortKeys: !settings.sortKeys })}
-        />
+          <label>{t.settings.labels.sortKeys}</label>
+          <input
+            className="self-center"
+            type="checkbox"
+            checked={settings.sortKeys}
+            onChange={() => updateSettings({ sortKeys: !settings.sortKeys })}
+          />
+        </div>
+
+        <div className="flex items-center justify-center pt-6">
+          <button
+            className="p-1.5 border rounded-lg text-red-900 dark:text-red-300 border-red-800 bg-red-800 bg-opacity-10 hover:bg-opacity-20 "
+            onClick={() => updateSettings(DefaultSettings)}
+          >
+            {t.settings.reset}
+          </button>
+        </div>
       </div>
     </TranslationContext.Provider>
   );
