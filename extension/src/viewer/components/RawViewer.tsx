943296256d148f8ac02eaf61dd67077b419b9c27
Paolo Simone <paolo.simone@prima.it>
2022-03-05 10:22:46 +0100

    select all

--- extension/src/viewer/components/RawViewer.tsx
+++ extension/src/viewer/components/RawViewer.tsx
@@ -1,5 +1,13 @@
 import classNames from "classnames";
-import { useCallback, useContext, useMemo, useState } from "react";
+import {
+  RefObject,
+  useCallback,
+  useContext,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
 import { EventType } from "viewer/commons/EventBus";
 import * as Json from "viewer/commons/Json";
 import { useEventBusListener, useHighlightedSearchResults } from "viewer/hooks";
@@ -33,14 +41,45 @@ export function RawViewer({
 
   const highlightedText = useHighlightedSearchResults(raw, search);
 
+  const ref = useRef<HTMLDivElement>(null);
+  useSelectAllText(ref);
+
   const wrap = minify ? "break-all" : "whitespace-pre";
 
   return (
     <div
+      ref={ref}
       className={classNames("overflow-auto", wrap, className)}
       spellCheck={false}
     >
       {highlightedText}
     </div>
   );
+}
+
+function useSelectAllText(ref: RefObject<HTMLElement>) {
+  useEffect(() => {
+    function selectAll(e: KeyboardEvent) {
+      if (!ref.current) {
+        return;
+      }
+
+      if ((e.ctrlKey || e.metaKey) && e.key == "a") {
+        e.preventDefault();
+        selectAllText(ref.current);
+      }
+    }
+
+    document.addEventListener("keydown", selectAll);
+    return () => document.removeEventListener("keydown", selectAll);
+  }, [ref]);
+}
+
+function selectAllText(node: HTMLElement) {
+  const range = document.createRange();
+  range.selectNode(node);
+
+  const selection = window.getSelection();
+  selection?.removeAllRanges();
+  selection?.addRange(range);
 }
