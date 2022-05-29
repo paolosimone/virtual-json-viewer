ca090be3424cbba9b6fc96fe07fd2c97187a276e
Paolo Simone <paolo.simone@prima.it>
2022-05-29 16:33:21 +0200

    handle settings upgrade

--- extension/src/viewer/hooks/UseSettings.ts
+++ extension/src/viewer/hooks/UseSettings.ts
@@ -9,6 +9,11 @@ export type DispatchSettings = (newSettings: Partial<Settings>) => void;
 export function useSettings(): [Settings, DispatchSettings] {
   const [settings, setSettings] = useStorage<Settings>(KEY, DefaultSettings);
 
+  const upgraded = maybeUpgrade(settings);
+  if (upgraded) {
+    setSettings(upgraded);
+  }
+
   const updateSettings = useMemo(
     () => (newSettings: Partial<Settings>) => {
       setSettings({ ...settings, ...newSettings });
@@ -17,4 +22,24 @@ export function useSettings(): [Settings, DispatchSettings] {
   );
 
   return [settings, updateSettings];
+}
+
+function maybeUpgrade(settings: Settings): Nullable<Settings> {
+  // changes in the same version are backward compatible
+  if (isLatestVersion(settings) && haveMissingKeys(settings)) {
+    return { ...DefaultSettings, ...settings };
+  }
+
+  // ...handle breaking changes
+
+  // nothing to do
+  return null;
+}
+
+function isLatestVersion(settings: Settings): boolean {
+  return settings.version == DefaultSettings.version;
+}
+
+function haveMissingKeys(settings: Settings): boolean {
+  return Object.keys(settings).length < Object.keys(DefaultSettings).length;
 }
