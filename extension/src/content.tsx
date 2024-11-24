e5e77a55e2eeaba2d079274381eeefb2bb2821ad
Paolo Simone <p.simone@live.com>
2024-11-24 17:02:36 +0100

    Add "force activation" setting (#54)

--- extension/src/content.tsx
+++ extension/src/content.tsx
@@ -1,120 +1,16 @@
-import { createRoot } from "react-dom/client";
-import { App as ViewerApp } from "./viewer/App";
+import * as DomEvents from "content/DomEvents";
+import * as Loader from "content/Loader";
+import * as Activator from "content/Activator";
 
-if (isJson()) {
-  afterHeadAvailable(setupResources);
-  afterDocumentLoaded(loadJsonViewer);
-}
-
-function isJson(): boolean {
-  // see: https://www.iana.org/assignments/media-types/media-types.xhtml
-  return (
-    document.contentType.startsWith("application/") &&
-    (document.contentType.endsWith("json") ||
-      document.contentType.endsWith("json-seq"))
-  );
-}
-
-function afterHeadAvailable(callback: () => void) {
-  if (document.head) {
-    callback();
-    return;
-  }
-
-  const observer = new MutationObserver((_mutations, observer) => {
-    if (document.head) {
-      observer.disconnect();
-      callback();
-    }
-  });
-  observer.observe(document, { childList: true, subtree: true });
-}
-
-function afterDocumentLoaded(callback: () => void) {
-  if (document.readyState === "complete") {
-    callback();
-    return;
-  }
-
-  window.addEventListener("load", callback);
-}
-
-function setupResources() {
-  if (document.readyState === "loading") {
-    setLoading(true);
-  }
-
-  addFavicon(chrome.runtime.getURL("logo/16.png"));
-  updateTitle();
-
-  addCSSRef(chrome.runtime.getURL("static/css/content.css"));
-}
-
-function loadJsonViewer() {
-  const jsonElement = document.getElementsByTagName("pre")[0];
-  const jsonText = jsonElement.innerText;
-
-  const div = document.createElement("div");
-  div.style.cssText = "height: 100vh";
-  jsonElement.parentNode?.replaceChildren(div);
-
-  setLoading(false);
-
-  createRoot(div).render(<ViewerApp jsonText={jsonText} />);
-}
-
-function setLoading(isLoading: boolean) {
-  setLoadingMessage(isLoading);
-  // workaround: improve loading time on large json
-  setTextVisibility(!isLoading);
-}
-
-function setLoadingMessage(show: boolean) {
-  if (show) {
-    const elem = document.createElement("div");
-    elem.id = "loading";
-    elem.innerText = "Loading...";
-    document.body.appendChild(elem);
-  } else {
-    const elem = document.getElementById("loading");
-    elem?.parentNode?.removeChild(elem);
-  }
-}
-
-function setTextVisibility(visible: boolean) {
-  if (visible) {
-    const elem = document.getElementById("hide-text");
-    elem?.parentNode?.removeChild(elem);
-  } else {
-    addCSS("pre { display: none; }", "hide-text");
-  }
-}
-
-function addCSS(style: string, id?: string) {
-  const styleElement = document.createElement("style");
-  styleElement.innerHTML = style;
-  if (id) styleElement.id = id;
-  document.head.appendChild(styleElement);
-}
-
-function addCSSRef(href: string) {
-  const linkElement = document.createElement("link");
-  linkElement.href = href;
-  linkElement.type = "text/css";
-  linkElement.rel = "stylesheet";
-  document.head.appendChild(linkElement);
-}
-
-function addFavicon(href: string) {
-  const linkElement = document.createElement("link");
-  linkElement.href = href;
-  linkElement.rel = "icon";
-  document.head.appendChild(linkElement);
-}
-
-function updateTitle() {
-  const segments = window.location.pathname.split("/");
-  // handle potential trailing slash
-  const last = segments.pop() || segments.pop();
-  if (last) document.title = last;
+if (Activator.isJsonContentType()) {
+  DomEvents.afterHeadAvailable(Loader.setupResources);
+  DomEvents.afterDocumentLoaded(Loader.loadViewer);
+} else {
+  Activator.checkSettings()
+    .then((forceActivation) => {
+      if (forceActivation) {
+        DomEvents.afterDocumentLoaded(Loader.forceSetupAndLoadViewer);
+      }
+    })
+    .catch((reason) => console.error(reason));
 }
--- extension/src/viewer/hooks/UseStorage.ts
+++ extension/src/viewer/hooks/UseStorage.ts
@@ -1,119 +1,36 @@
 import { Dispatch, useEffect, useMemo, useState } from "react";
-import { RUNTIME, Runtime } from "viewer/state";
+import { STORAGE } from "viewer/commons/Storage";
 import { Mutex, useEffectAsync } from ".";
 
 export function useStorage<T>(key: string, defaultValue: T): [T, Dispatch<T>] {
-  const storage = RUNTIME === Runtime.Extension ? SYNC_STORAGE : LOCAL_STORAGE;
-
   const [value, setValue] = useState(defaultValue);
 
   // initialize value on first render
   useEffectAsync(
     async (mutex: Mutex) => {
-      let value: Nullable<T> = await storage.get<T>(key);
+      let value: Nullable<T> = await STORAGE.get<T>(key);
       if (!mutex.hasLock()) {
         return;
       }
       if (value === null) {
-        await storage.set(key, defaultValue);
+        await STORAGE.set(key, defaultValue);
         value = defaultValue;
       }
       setValue(value);
     },
-    [storage, key, defaultValue],
+    [key, defaultValue],
   );
 
   // subscribe to external changes
-  useEffect(() => storage.addListener(key, setValue), [storage, key, setValue]);
+  useEffect(() => STORAGE.addListener(key, setValue), [key, setValue]);
 
   // function to update both storage and internal state
   const setValueAndStorage = useMemo(
     () => (newValue: T) => {
-      storage.set(key, newValue).then(() => setValue(newValue));
+      STORAGE.set(key, newValue).then(() => setValue(newValue));
     },
-    [storage, key, setValue],
+    [key, setValue],
   );
 
   return [value, setValueAndStorage];
 }
-
-type RemoveListener = () => void;
-
-interface Storage {
-  get<T>(key: string): Promise<Nullable<T>>;
-  set<T>(key: string, item: T): Promise<void>;
-  addListener<T>(key: string, onChange: Dispatch<T>): RemoveListener;
-}
-
-class LocalStorage implements Storage {
-  get<T>(key: string): Promise<Nullable<T>> {
-    return new Promise((resolve, reject) => {
-      try {
-        const value = localStorage.getItem(key);
-        resolve(value !== null ? JSON.parse(value) : null);
-      } catch (e) {
-        reject(e);
-      }
-    });
-  }
-
-  set<T>(key: string, item: T): Promise<void> {
-    return new Promise((resolve, reject) => {
-      try {
-        localStorage.setItem(key, JSON.stringify(item));
-        resolve();
-      } catch (e) {
-        reject(e);
-      }
-    });
-  }
-
-  addListener<T>(key: string, onChange: Dispatch<T>): RemoveListener {
-    function listener(e: StorageEvent) {
-      if (e.key === key) {
-        const newValue = e.newValue !== null ? JSON.parse(e.newValue) : null;
-        onChange(newValue);
-      }
-    }
-
-    window.addEventListener("storage", listener);
-    return () => window.removeEventListener("storage", listener);
-  }
-}
-
-const LOCAL_STORAGE = new LocalStorage();
-
-class SyncStorage implements Storage {
-  get<T>(key: string): Promise<Nullable<T>> {
-    return new Promise((resolve, reject) => {
-      try {
-        chrome.storage.sync.get(key, (items) => resolve(items[key] ?? null));
-      } catch (e) {
-        reject(e);
-      }
-    });
-  }
-
-  set<T>(key: string, item: T): Promise<void> {
-    return new Promise((resolve, reject) => {
-      try {
-        chrome.storage.sync.set({ [key]: item }, resolve);
-      } catch (e) {
-        reject(e);
-      }
-    });
-  }
-
-  addListener<T>(key: string, onChange: Dispatch<T>): RemoveListener {
-    function listener(changes: { [key: string]: any }) {
-      if (key in changes) {
-        onChange(changes[key].newValue);
-      }
-    }
-
-    chrome.storage.onChanged.addListener(listener);
-    return () => chrome.storage.onChanged.removeListener(listener);
-  }
-}
-
-const SYNC_STORAGE = new SyncStorage();
